0707b3c5edc5926dc8ab350addfe3d65280c664c
kravtandr <kravtandr@gmail.com>
2021-12-05 17:39:22 +0300

    set domain

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,12 +1,9 @@
 // export const backendEndpoint = 'http://194.58.104.204:8080';
-// export const backendFileEndpoint = 'http://194.58.104.204:3000/';
 // export const frontEndEndPoint = 'http://194.58.104.204';
 
 // export const backendEndpoint = 'http://localhost:8080';
-// export const backendFileEndpoint = 'http://localhost:3000/';
 // export const frontEndEndPoint = 'http://localhost:2000';
 
 export const backendEndpoint = 'https://snakehastrip.ru';
-export const backendFileEndpoint = 'https://snakehastrip.ru';
 export const frontEndEndPoint = 'https://snakehastrip.ru';
 
