3bc1b9d0fe07199ea9bf05aa194352c9426cab73
kravtandr <kravtandr@gmail.com>
2021-12-16 15:33:14 +0300

    .

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -11,4 +11,4 @@ export const backendEndpoint = 'https://snakehastrip.ru/api';
 export const backendFileEndpoint = 'https://snakehastrip.ru/api';
 export const frontEndEndPoint = 'https://snakehastrip.ru';
 //export const WSEndPoint = "https://snakehastrip.ru/apiws/connect"
-export const WSEndPoint = "ws://snakehastrip.ru/apiws/connect"
+export const WSEndPoint = "wss://snakehastrip.ru/apiws/connect"
